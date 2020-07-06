f89f6514a3e2c4c878fed6ce97d1b8fd5f4c36c0
Pankaj Kumar Singh <pankaj28843@gmail.com>
2020-07-06 14:01:51 +0000

    chore: add 3rdparty addons

---
+++ 3rd_party_addons/Odoo-App-Auth-OAuth-Keycloak/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Auth OAuth Keycloak",
+
+    'summary': """
+        Enables Keycloack integration for the OAuth2 Authentication module.""",
+
+    'description': """
+        Enables Keycloack integration for the OAuth2 Authentication module.
+    """,
+
+    'author': "Mint System",
+    'website': "https://www.mint-system.ch",
+
+    'category': 'Technical Settings',
+    'version': '1.0.0',
+
+    'depends': [
+        'base',
+        'auth_oauth',
+    ],
+
+    'data': [
+        'views/ir_model_fields.xml',
+        'views/ir_ui_view.xml',
+    ],
+}
